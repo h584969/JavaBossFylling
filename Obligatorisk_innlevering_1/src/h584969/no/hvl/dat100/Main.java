712087c65e5112607b14e0b2dee0c5d0b5d857ed
Eier <h584969@stud.hvl.no>
2020-09-02 11:45:04 +0200

    lagt til den obligatoriske oppgaven

---
+++ Obligatorisk_innlevering_1/src/h584969/no/hvl/dat100/Main.java
@@ -0,0 +1,17 @@
+package h584969.no.hvl.dat100;
+
+import h584969.no.hvl.dat100.oppgave4.Trinnskatt;
+import h584969.no.hvl.dat100.oppgave5.Karakterskala;
+import h584969.no.hvl.dat100.oppgave6.Fakultetkalkulator;
+
+public class Main {
+	public static void main(String[] args) {
+		//alle oppgavene ha sin egen klasse
+		
+		Trinnskatt.mainTrinnskatt(args);
+		
+		Karakterskala.mainKarakterskala(args);
+		
+		Fakultetkalkulator.mainFakultetKalkulator(args);
+	}
+}
---
+++ Obligatorisk_innlevering_1/src/h584969/no/hvl/dat100/oppgave6/Fakultetkalkulator.java
@@ -0,0 +1,24 @@
+package h584969.no.hvl.dat100.oppgave6;
+
+import javax.swing.JOptionPane;
+
+public class Fakultetkalkulator {
+	
+	public static void mainFakultetKalkulator(String[] args) {
+			JOptionPane.showMessageDialog(null, "Oppgave 6");
+			
+			int n = Integer.parseInt(JOptionPane.showInputDialog("gi et positivt heltall"));
+			
+			if (n < 0) {
+				JOptionPane.showMessageDialog(null, "Tallet må være over 0");
+			}
+			
+			int sum = 1;
+			
+			for (int i = 1; i <= n; i++) {
+				sum *= i;
+			}
+			
+			JOptionPane.showMessageDialog(null, n + "! = " + sum);
+	}
+}
